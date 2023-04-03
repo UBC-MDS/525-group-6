2c201831ca6971abfeebc1b27177a9ba95621897
Chester Wang <chesteraigo@dhcp-206-12-41-198.ubcsecure.wireless.ubc.ca>
2023-04-03 15:35:29 -0700

    updated merge script to upload s3 directly

--- milestone2/scripts/merge.py
+++ milestone2/scripts/merge.py
@@ -1,16 +1,30 @@
 import pandas as pd
+import s3fs
+
+# Load raw data
 filepathparquet = "./combined_model_data_parti.parquet"
 df = pd.read_parquet(filepathparquet)
+
+# Filter to keep Sydney data only
 syd_lat = -33.86
 syd_lon = 151.21
 
 df = df[(df['lat_min'] < syd_lat) & (df['lat_max'] > syd_lat)]
 df = df[(df['lon_min'] < syd_lon) & (df['lon_max'] > syd_lon)]
+
+# Drop redundant columns
 df = df.drop(['lat_min', 'lat_max', 'lon_min', 'lon_max'], axis=1)
 
+# Re-format time
 df['time'] = pd.to_datetime(df['time'])
 df['time'] = df['time'].dt.date
+
+# Pivot dataframe
 df = df.pivot_table(index='time', columns='model', values='rain (mm/day)')
 
-df.to_csv('./ml_data_SYD.csv')
+# Save to csv
+s3_path = 's3://mds2023-group6/output/ml_data_SYD.csv'
+fs = s3fs.S3FileSystem()
 
+with fs.open(s3_path, 'w') as f:
+    df.to_csv(f)
